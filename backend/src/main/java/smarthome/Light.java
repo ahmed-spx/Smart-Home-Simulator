90b3e9819d2443bb5cae84dc996138f94a21ca58
Ahmed Al Saad <ahmed.q2099@gmail.com>
2026-04-21 16:54:49 -0400

    added brightness and color to light

--- backend/src/main/java/smarthome/Light.java
+++ backend/src/main/java/smarthome/Light.java
@@ -1,8 +1,14 @@
 package main.java.smarthome;
+import java.awt.Color;
 
 public class Light extends Device implements IPoweredDevice{
   private boolean isOn;
+
+  private int brightness;
+  final int limitedValue = Math.clamp(brightness, 10, 100);
   
+  private Color color = new Color(255, 255, 255);
+
   public Light(int id, String name, String location, DeviceType type) {
     super(id, name, location, type);
     this.isOn = false;
