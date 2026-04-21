2b2f70a38d83f1dc05c49dbcaa88e2fb52290f39
Ahmed Al Saad <ahmed.q2099@gmail.com>
2026-04-21 19:44:09 -0400

    light brightness and color methods

--- backend/src/main/java/smarthome/Light.java
+++ backend/src/main/java/smarthome/Light.java
@@ -4,14 +4,24 @@ import java.awt.Color;
 public class Light extends Device implements IPoweredDevice{
   private boolean isOn;
 
-  private int brightness;
-  final int limitedValue = Math.clamp(brightness, 10, 100);
-  
+  private int brightness = 100;
   private Color color = new Color(255, 255, 255);
 
   public Light(int id, String name, String location, DeviceType type) {
     super(id, name, location, type);
     this.isOn = false;
+  }
+
+  public void setBrightness(int brightness) {
+    if (brightness < 10 || brightness > 100) {
+      throw new IllegalArgumentException("Brightness must be between 10 and 100");
+    } else {
+      this.brightness = brightness;
+    }
+  }
+
+  public void setColor(Color color) {
+    this.color = color;
   }
 
   @Override
