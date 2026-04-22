2c718c97ded74d17bcfb6dbf63e43b03669ac810
Ahmed Al Saad <ahmed.q2099@gmail.com>
2026-04-21 20:03:02 -0400

    updated fan class

--- backend/src/main/java/smarthome/Fan.java
+++ backend/src/main/java/smarthome/Fan.java
@@ -2,10 +2,15 @@ package main.java.smarthome;
 
 public class Fan extends Device implements IPoweredDevice {
   private boolean isOn;
+  private int speed; // 0 for off, 1 for low, 2 for medium, 3 for high
 
   public Fan(int id, String name, String location, DeviceType type) {
     super(id, name, location, type);
     this.isOn = false;
+  }
+
+  public void setSpeed(int speed) {
+    this.speed = speed;
   }
 
   @Override
