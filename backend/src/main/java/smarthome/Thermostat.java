2579755275eb7494241b0a2b17111b15387f8ee8
Ahmed Al Saad <ahmed.q2099@gmail.com>
2026-04-21 20:06:19 -0400

    quick thermostat

--- backend/src/main/java/smarthome/Thermostat.java
+++ backend/src/main/java/smarthome/Thermostat.java
@@ -2,11 +2,21 @@ package main.java.smarthome;
 
 public class Thermostat extends Device implements IPoweredDevice {
   private boolean isOn;
+  private int mode;
+  private double desiredTemperature;
 
 
   public Thermostat(int id, String name, String location, DeviceType type) {
     super(id, name, location, type);
     this.isOn = false;
+  }
+
+  public void setMode(int mode) {
+    this.mode = mode;
+  }
+
+  public void setTemperature(double temperature) {
+    desiredTemperature = temperature;
   }
 
   @Override
