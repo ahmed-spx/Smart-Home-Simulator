37d5bc7f8904786274dca03a3bf46be2298a0901
Ahmed Al Saad <ahmed.q2099@gmail.com>
2026-05-07 11:49:10 -0400

    adding import

--- backend/src/main/java/smarthomesim/demo/Thermostat/ThermostatStates/HeatingThermostatState.java
+++ backend/src/main/java/smarthomesim/demo/Thermostat/ThermostatStates/HeatingThermostatState.java
@@ -2,6 +2,7 @@ package smarthomesim.demo.Thermostat.ThermostatStates;
 import smarthomesim.demo.Thermostat.IThermostatState;
 import smarthomesim.demo.Thermostat.Thermostat;
 import smarthomesim.demo.Thermostat.ThermostatState;
+import smarthomesim.demo.Thermostat.ThermostatMode;
 
 public class HeatingThermostatState implements IThermostatState {
 
